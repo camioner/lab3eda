4fd559c7c916deb61e7d0975c6bd44114ed98552
camioner <cris_to515@hotmail.com>
2025-05-27 18:15:19 -0400

    falta crear el archivo y probar el codigo

--- src/lab3/Main.java
+++ src/lab3/Main.java
@@ -2,7 +2,7 @@ package lab3;
 
 import java.util.ArrayList;
 
-public class Main {
+public class main {
 
     public static void main(String[] args) {
         int[] tamanos = {100, 10000, 1000000};
